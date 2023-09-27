aeb9e8d2a29a8900cad23412c494912d71ae9cd7
elijah629 <62805599+elijah629@users.noreply.github.com>
2023-09-27 14:45:37 -0700

    fix: top level await

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import solid from "solid-start/vite";
 import wasm from "vite-plugin-wasm";
-//import topLevelAwait from "vite-plugin-top-level-await";
+import topLevelAwait from "vite-plugin-top-level-await";
 import vercel from "solid-start-vercel";
 
 import { defineConfig } from "vite";
 import path from "node:path";
 
 export default defineConfig({
-	plugins: [solid({ adapter: vercel() }), wasm()/*, topLevelAwait()*/],
+	plugins: [solid({ adapter: vercel() }), wasm(), topLevelAwait()],
 	build: {
 		target: "esnext"
 	},
